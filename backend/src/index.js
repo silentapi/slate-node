4351eaeb15243efbbdececc8c34f3fb7e5593a01
Joe Winkler <joe.c.winkler@outlook.com>
2020-07-19 19:07:22 -0400

    Add MongoDB connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,23 @@
 import 'dotenv/config';
 
 import express from 'express';
+import mongoose from 'mongoose';
 import {json} from 'body-parser';
 import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO = process.env.MONGO || 'mongodb://127.0.0.1:27017/slate';
 
 app.use(cors());
 app.use(json());
+
+mongoose.connect(MONGO, {useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+  console.log('MongoDB database connection established successfully');
+});
 
 app.get('/', function(req, res) {
   res.send('Hello, World!');
